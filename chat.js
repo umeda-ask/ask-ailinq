27b880bdcf5e4ff90988796a74123359834fc3b7
DESKTOP-F6K8NP4\umeda <h.umeda@askpro.co.jp>
2024-09-18 10:47:49 +0900

    文言修正

--- chat.js
+++ chat.js
@@ -288,7 +288,7 @@ const chatList = {
     6: { text: 'お名前をフルネームで入力して送信ボタンを押してください', continue: false, option: 'normal', return: false },
     7: { text: 'ご自宅の住所を入力して送信ボタンを押してください。(例:東京都千代田区丸の内)', continue: false, option: 'normal', return: false },
     8: { text: '専門家のご連絡を受け取れるお電話番号を入力して送信ボタンを押してください。(例:0901111XXXX)', continue: false, option: 'normal', return: false },
-    9: { text: 'お問い合わせ内容を入力して送信ボタンを押してください。(①問題が発生した日時や場所、\r\n②関係者、\r\n③これまでに行った対応、\r\n④望む解決方法をご入力ください)', continue: false, option: 'normal', return: false },
+    9: { text: 'お問い合わせ内容を入力して送信ボタンを押してください。※AIによるフィルタリングを実施しており、内容によってはお答えすることが出来ません。以下に注意して相談をご入力ください。\r\n①問題が発生した日時や場所\r\n②関係者\r\n③これまでに行った対応\r\n④望む解決方法をご入力ください', continue: false, option: 'normal', return: false },
     10: { text: { title: '相談を送信しますか？', choices: ['はい', '入力しなおす'] }, continue: false, option: 'choices', return: false },
     11: { text: 'ご利用ありがとうございました。\nご入力いただいた情報を元に対応出来る専門家をお探しします。見つかった場合は3営業日以内に専門家からのご連絡がございます。\nお急ぎの場合は直接、専門家へのご連絡お願い致します。\r\nまた、ご入力いただいた情報につきましては弊社では保持しておりません。', continue: false, option: 'normal', return: true },
     12: { text: 'もう一度相談内容を入力してください。', continue: false, option: 'normal', return: false },
